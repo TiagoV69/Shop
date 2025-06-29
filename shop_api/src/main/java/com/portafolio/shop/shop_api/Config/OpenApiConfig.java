087358c2a387a7758e6af87d65c21cf7f1537940
TiagoV69 <santyvarela.03@gmail.com>
2025-06-28 19:59:10 -0500

    feat(docs): Integración de OpenAPI 3 para la documentación automática de la API
    Se ha añadido la dependencia "springdoc-openapi-starter-webmvc-ui" para generar documentación interactiva de la API.
    
    Se ha configurado una clase "OpenApiConfig" para personalizar los metadatos de la documentacion, incluyendo el título, la descripcion y la licencia de la API.
    
    La documentacion ahora esta disponible en la ruta "/swagger-ui.html", proporcionando una interfaz visual para explorar y probar todos los endpoints de la aplicacion.

---
+++ shop_api/src/main/java/com/portafolio/shop/shop_api/Config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.portafolio.shop.shop_api.Config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tienda Online API")
+                        .version("1.0.0")
+                        .description("API REST para una tienda online de ejemplo, construida con Spring Boot. Este proyecto demuestra la gestión de productos, usuarios con roles y un sistema de pedidos.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
